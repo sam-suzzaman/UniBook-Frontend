74ff57b93f45c89c8574d302cb7fe75417dd3744
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2024-04-29 08:15:08 +0600

    refactor code

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -8,6 +8,7 @@ const baseApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: "https://uni-book.vercel.app/api/v1",
         credentials: "include",
+        mode: "no-cors",
     }),
 
     endpoints: (builder) => ({
